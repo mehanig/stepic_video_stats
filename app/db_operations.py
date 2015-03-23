fbd313d8893e888884b834283e96826c803224c8
mehanig <mehanig@gmail.com>
2015-03-23 19:17:44 +0300

    new Version fixes

--- app/db_operations.py
+++ app/db_operations.py
@@ -1,1 +1,10 @@
-__author__ = 'mehanig'
+def delete_entry_by_name(name, db_collection):
+    print(name)
+    try:
+        if db_collection.find({'data.Name': name}).count() > 1:
+            raise Exception("Name entries collapse. Names not unique at", db_collection, "data.Name:", name)
+    except Exception as e:
+        return e
+    status = db_collection.remove({'data.Name': name})
+    print('DELETED! ', status)
+    return True
